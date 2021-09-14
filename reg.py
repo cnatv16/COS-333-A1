894abb733dc8ced6a3507517ec471cd28e6543b9
cnatv16 <90236261+cnatv16@users.noreply.github.com>
2021-09-14 19:13:31 -0400

    Add files via upload

---
+++ reg.py
@@ -0,0 +1,29 @@
+#!/usr/bin env python
+
+#---------------------------------------------------------------------------------
+# reg.py
+# Authors: Camila Vasquez & Tanzila Morshed
+#---------------------------------------------------------------------------------
+
+from sys import argv, stderr, exit
+from contextlib import closing
+from sqlite3 import connect
+
+#---------------------------------------------------------------------------------
+
+DATABASE_URL = 'file: reg.sqlite?mode=ro'
+
+def main():
+
+    #if len(argv) == 0:
+     #   print('Usage: python reg.py', file=stderr)
+     #   exit(1)
+
+    if argv[1] == '-h':
+        print('usage: reg.py [-h] [-d dept] [-n num] [-a area] [-t title] \n')
+        print('Registrar application: show overviews of classes \n') 
+        exit(0)
+
+# try:with connect (DATABASE_URL, uri = True) as connection:
+
+          
