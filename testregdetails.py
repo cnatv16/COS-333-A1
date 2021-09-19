dafe165fa1559b676920dab06d95793968c21385
cnatv16 <cnatv16@gmail.com>
2021-09-19 16:07:02 -0400

    Create testregdetails.py

---
+++ testregdetails.py
@@ -0,0 +1,20 @@
+#!/usr/bin env python
+
+#---------------------------------------------------------------------------------
+# testregdetails.py
+# Authors: Camila Vasquez & Tanzila Morshed
+#---------------------------------------------------------------------------------
+
+from sys import argv, stderr, exit
+import os
+#from contextlib import closing
+#from sqlite3 import connect
+#import argparse, re, textwrap
+#---------------------------------------------------------------------------------
+
+os.system('python regdetails.py 8321')
+os.system('python regdetails.py 9032')
+os.system('python regdetails.py 9977')
+os.system('python regdetails.py 9012')
+os.system('python regdetails.py 10188')
+
