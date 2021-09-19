0bb79d02fd0415c5585f5fda07f3c21cc2ac1515
Tanzila Morshed <youremailaddress>
2021-09-18 21:00:43 -0400

    updated if statements

--- reg.py
+++ reg.py
@@ -32,7 +32,10 @@ def main():
             with closing (connection.cursor()) as cursor:
 
                 stmt_str = "SELECT classid, dept, coursenum, area, title FROM classes, crosslistings, courses WHERE (courses.courseid = classes.courseid) "
-                stmt_str += "AND (classes.courseid = crosslistings.courseid) AND (dept LIKE ?) AND (coursenum LIKE ?) AND (area LIKE ?)" 
+                stmt_str += "AND (classes.courseid = crosslistings.courseid) " 
+                stmt_str += r"AND (dept LIKE ? ESCAPE '\') "
+                stmt_str += r"AND (coursenum LIKE ? ESCAPE '\') " 
+                stmt_str += r"AND (area LIKE ? ESCAPE '\') " 
                 stmt_str += r"AND (title LIKE ? ESCAPE '\')"
 
                 dept= "%%"
@@ -41,33 +44,47 @@ def main():
                 title = "%%"
 
                 if args.d != None:
-                    dept = '%' + str(args.d) + '%'
-                    # escape character ifs
+                    if '%' in str(args.d):
+                        dep = args.d.replace("%", r"\%")
+                        dept = "%" + dep + "%"
+                    elif '_' in str(args.d):
+                        dep = args.d.replace("_", r"\_")
+                        dept = "%" + dep + "%"
+
+                    else:
+                        dept = '%' + str(args.d) + '%'
 
                 if args.n != None:
                     num = '%' + str(args.n) + '%'
  
 
                 if args.a != None:
-                    area = '%' + str(args.a) + '%'
+                    if '%' in str(args.a):
+                        ar = args.a.replace("%", r"\%")
+                        area = "%" + ar + "%"
+                    elif '_' in str(args.d):
+                        ar = args.a.replace("_", r"\_")
+                        area = "%" + ar + "%"
+                    else:
+                        area = '%' + str(args.a) + '%'
                     # escape character ifs
         #replace for underscore as well
         # do replace for other queries as well
         # hardcode sql statement, set each var initially to "%%", change in if statements
 
                 if args.t != None:
-                    if '%' not in str(args.t):
-                        title = '%' + str(args.t) + '%'
-                    else:
+                    if '%' in str(args.t):
                         tit = args.t.replace("%", r"\%")
-                        title = "%" + tit + "%"  #+ " ESCAPE '\'"
-                        print(title)
-
-                    #stmt_str += r"AND (title LIKE ? ESCAPE '\') "
+                        title = "%" + tit + "%"
+                    elif '_' in str(args.t):
+                        tit = args.t.replace("_", r"\_")
+                        title = "%" + tit + "%"
+                    else:
+                        title = '%' + str(args.t) + '%'
                     print(stmt_str)
                 
                 print(title)
-                #print(prep)
+
                 cursor.execute(stmt_str, [dept, num, area, title])
 
                 row = cursor.fetchone()
