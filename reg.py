59d9d6ecc90ee3475e7e8a7bcc40b78148619c42
Tanzila Morshed <youremailaddress>
2021-09-22 17:51:32 -0400

    final submitted assignment

--- reg.py
+++ reg.py
@@ -1,55 +1,67 @@
 #!/usr/bin env python
 
-#---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 # reg.py
 # Authors: Camila Vasquez & Tanzila Morshed
-#---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
-from sys import argv, stderr, exit
+from sys import stderr, exit
 from contextlib import closing
 from sqlite3 import connect
-import argparse, textwrap
-#---------------------------------------------------------------------------------
+import argparse
+import textwrap
+#-----------------------------------------------------------------------
 
 DATABASE_URL = 'file:reg.sqlite?mode=ro'
 
 def main():
-    # using argseparse to manage the potential arugemnts for this program, explaining each argument's function
-    parser = argparse.ArgumentParser(description="Registrar application: show overviews of classes",  allow_abbrev=False)
-    parser.add_argument("-d", metavar= "dept", dest = "d", help="show only those classes whose department contains dept", 
-                        action="store")
-    parser.add_argument("-n", metavar="num", dest = "n", help="show only those classes whose course number contains num", 
-                        action="store")
-    parser.add_argument("-a", metavar="area", dest = "a", help="show only those classes whose distrib area contains area", 
-                        action="store")
-    parser.add_argument("-t", metavar="title", dest = "t", help="show only those classes whose course title contains title", 
-                        action="store")
+    # using argseparse to manage the potential arugemnts for this 
+    # program, explaining each argument's function
+    parser = argparse.ArgumentParser(description="Registrar \
+        application: show overviews of classes",  allow_abbrev=False)
+    parser.add_argument("-d", metavar= "dept", dest = "d", help="show \
+        only those classes whose department contains dept", \
+        action="store")
+    parser.add_argument("-n", metavar="num", dest = "n", help="show \
+        only those classes whose course number contains num", \
+        action="store")
+    parser.add_argument("-a", metavar="area", dest = "a", help="show \
+        only those classes whose distrib area contains area", 
+        action="store")
+    parser.add_argument("-t", metavar="title", dest = "t", help="show \
+        only those classes whose course title contains title",
+        action="store")
     args = parser.parse_args()
 
 
     try:
-         # connecting to the database, if fails closes and throws exception
+         # connecting to the database, if fails closes and throws 
+         # exception
         with connect (DATABASE_URL, uri=True) as connection:
             with closing (connection.cursor()) as cursor:
                 
-                # populating the statement to pass into the cursor and search through the database
-                stmt_str = "SELECT classid, dept, coursenum, area, title FROM classes, crosslistings, courses WHERE (courses.courseid = classes.courseid) "
-                stmt_str += "AND (classes.courseid = crosslistings.courseid) " 
+                # populating the statement to pass into the cursor 
+                # and search through the database
+                stmt_str = "SELECT classid, dept, coursenum, area, \
+                    title FROM classes, crosslistings, courses \
+                        WHERE (courses.courseid = classes.courseid) "
+                stmt_str += "AND (classes.courseid = \
+                    crosslistings.courseid) " 
                 stmt_str += r"AND (dept LIKE ? ESCAPE '\') "
                 stmt_str += r"AND (coursenum LIKE ? ESCAPE '\') " 
                 stmt_str += r"AND (area LIKE ? ESCAPE '\') " 
                 stmt_str += r"AND (title LIKE ? ESCAPE '\')"
                 stmt_str += "ORDER BY dept, coursenum, classid ASC"
 
-               
                 dept= "%%"
                 num = "%%"
                 area = "%%"
                 title = "%%"
 
-                # initialize variables, dept, num, area, and title based on given arguments
-                # accounting for escape characters potentially passed in through arguments 
-                if args.d != None:
+                # initialize variables, dept, num, area, and title 
+                # based on given arguments accounting for escape 
+                # characters potentially passed in through arguments 
+                if args.d is not None:
                     if '%' in str(args.d):
                         dept = "%" + args.d.replace("%", r"\%") + "%"
                     elif '_' in str(args.d):
@@ -58,10 +70,10 @@ def main():
                     else:
                         dept = '%' + str(args.d) + '%'
 
-                if args.n != None:
+                if args.n is not None:
                     num = '%' + str(args.n) + '%'
 
-                if args.a != None:
+                if args.a is not None:
                     if '%' in str(args.a):
                         area = "%" + args.a.replace("%", r"\%") + "%"
                     elif '_' in str(args.d):
@@ -69,7 +81,7 @@ def main():
                     else:
                         area = '%' + str(args.a) + '%'
 
-                if args.t != None:
+                if args.t is not None:
                     if '%' in str(args.t):
                         title = "%" + args.t.replace("%", r"\%") + "%"
                     elif '_' in str(args.t):
@@ -77,7 +89,8 @@ def main():
                     else:
                         title = '%' + str(args.t) + '%'
                    
-                # cursor executes the prepared statement to search through the database
+                # cursor executes the prepared statement to search 
+                # through the database
                 cursor.execute(stmt_str, [dept, num, area, title])
 
                 # first row from database
@@ -87,19 +100,28 @@ def main():
                 print("ClsId Dept CrsNum Area Title")
                 print("----- ---- ------ ---- -----")
 
-                # traverse through all the rows returned from the cursor execution
+                # traverse through all the rows returned from the 
+                # cursor execution
                 while row is not None:
-                    # using textwrap to populate text in requested format
+                    # using textwrap to populate text in requested 
+                    # format
                     text = ""
-                    text += textwrap.fill(str(row[0]), initial_indent = ' ' * (5-len(str(row[0]))))
-                    text += textwrap.fill(str(row[1]), initial_indent = ' ' * (5-len(str(row[1]))))
-                    text += textwrap.fill(str(row[2]), initial_indent = ' ' * (7-len(str(row[2]))))
-                    text += textwrap.fill(str(row[3]), initial_indent = ' ' * (5-len(str(row[3]))))
+                    text += textwrap.fill(str(row[0]), 
+                        initial_indent = ' ' * (5-len(str(row[0]))))
+                    text += textwrap.fill(str(row[1]), 
+                        initial_indent = ' ' * (5-len(str(row[1]))))
+                    text += textwrap.fill(str(row[2]), 
+                        initial_indent = ' ' * (7-len(str(row[2]))))
+                    text += textwrap.fill(str(row[3]), 
+                        initial_indent = ' ' * (5-len(str(row[3]))))
                     if row[3] != "":
-                        text += textwrap.fill(str(row[4]), initial_indent = ' ')
+                        text += textwrap.fill(str(row[4]), 
+                            initial_indent = ' ')
                     else:
-                        text += textwrap.fill(str(row[4]), initial_indent = ' ' * 6)
-                    print(textwrap.fill(text, subsequent_indent= ' ' * 23, width = 72))
+                        text += textwrap.fill(str(row[4]), 
+                            initial_indent = ' ' * 6)
+                    print(textwrap.fill(text, 
+                        subsequent_indent= ' ' * 23, width = 72))
                     # fetch the next row we want from the database
                     row = cursor.fetchone()
 
--- regdetails.py
+++ regdetails.py
@@ -1,99 +1,105 @@
 #!/usr/bin env python
 
-#---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 # regdetails.py
 # Authors: Camila Vasquez & Tanzila Morshed
-#---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
-from sys import argv, stderr, exit
+from sys import stderr, exit
 from contextlib import closing
 from sqlite3 import connect
-import argparse, textwrap, sys
-#---------------------------------------------------------------------------------
+import argparse
+import sys
+import textwrap
+#-----------------------------------------------------------------------
 DATABASE_URL = 'file:reg.sqlite?mode=ro'
 
 def main():
-    # build arg parser object, add arguments, and parse input
-    parser = argparse.ArgumentParser(description="Registrar application: \
-        show details about a class",  allow_abbrev=False)
-    parser.add_argument("classid", type = int, help="the id of the class whose details \
-        should be shown")
+    # build an arg parser object, add args, and parse input
+    parser = argparse.ArgumentParser(description="Registrar \
+        application: show details about a class",  allow_abbrev=False)
+    parser.add_argument("classid", type = int, help="the id of the \
+        class whose details should be shown")
     args = parser.parse_args()
 
     try:
-            with connect (DATABASE_URL, uri=True) as connection:
-                with closing (connection.cursor()) as cursor:
+        with connect (DATABASE_URL, uri=True) as connection:
+            with closing (connection.cursor()) as cursor:
+                # build sql statement strings for cursor     
+                if args.classid  != None:
+                    class_stmt = "SELECT classes.courseid, days, \
+                        starttime, endtime, bldg, roomnum FROM \
+                        classes, courses WHERE (courses.courseid \
+                        = classes.courseid) AND classid LIKE ?"
 
-                    # build sql statement strings for cursor 
-                    if args.classid  != None:
-                        class_stmt = "SELECT classes.courseid, days, starttime, \
-                            endtime, bldg, roomnum FROM classes, courses \
-                            WHERE (courses.courseid = classes.courseid) \
-                            AND classid LIKE ?"
+                    cross_stmt = "SELECT dept, coursenum FROM \
+                        crosslistings, courses, classes WHERE \
+                        (courses.courseid = crosslistings.courseid) \
+                        AND (courses.courseid = classes.courseid) \
+                        AND classid LIKE ? ORDER BY dept, \
+                        coursenum ASC"
 
-                        cross_stmt = "SELECT dept, coursenum FROM crosslistings, \
-                            courses, classes WHERE (courses.courseid = crosslistings.courseid) \
-                            AND (courses.courseid = classes.courseid) AND classid LIKE ? \
-                            ORDER BY dept, coursenum ASC"
+                    course_stmt = "SELECT area, title, descrip, \
+                        prereqs FROM courses, classes \
+                        WHERE (courses.courseid = classes.courseid) \
+                        AND classid LIKE ?"
 
-                        course_stmt = "SELECT area, title, descrip, prereqs FROM courses, classes \
-                            WHERE (courses.courseid = classes.courseid) AND classid LIKE ?"
+                    profs_stmt = "SELECT profname FROM profs, \
+                        coursesprofs, classes WHERE \
+                        (coursesprofs.courseid = classes.courseid) AND \
+                        (profs.profid = coursesprofs.profid) AND \
+                        classid LIKE ? ORDER BY profname ASC" 
+                # run sql request with given argument
+                cursor.execute(class_stmt,[args.classid])
+                row = cursor.fetchone()
 
-                        profs_stmt = "SELECT profname FROM profs, coursesprofs, classes \
-                            WHERE (coursesprofs.courseid = classes.courseid) AND \
-                            (profs.profid = coursesprofs.profid) AND classid LIKE ? \
-                            ORDER BY profname ASC" 
+                # throw exception if no such class exists
+                if row is None:
+                    raise RuntimeError
 
-                    # run sql request with given argument 
-                    cursor.execute(class_stmt,[args.classid])
+                # print details
+                print("Course Id:", row[0])
+                print()
+                print("Days:", row[1])
+                print("Start time:", row[2])
+                print("End time:", row[3])
+                print("Building:", row[4])
+                print("Room:", row[5])
+                print()
+
+                # run sql request for crosslistings and print 
+                cursor.execute(cross_stmt,[args.classid])
+                row = cursor.fetchone()
+                while row is not None:
+                    print("Dept and Number:", row[0],  row[1])
                     row = cursor.fetchone()
 
-                    # throw exception if no such class exists 
-                    if row is None:
-                        raise RuntimeError
+                # run sql statement for courses and print
+                cursor.execute(course_stmt,[args.classid])
+                row = cursor.fetchone()
+                print()
+                print("Area:", row[0])
+                print()
+                title = "Title: " + row[1]
+                print(textwrap.fill(title, width = 72))
+                print()
+                descrip = "Description: " + row[2]
+                print(textwrap.fill(descrip, width = 72))
+                print()
+                prereq = "Prerequisites: " + row[3]
+                print(textwrap.fill(prereq, width = 72))
+                print()
 
-                    # print details 
-                    print("Course Id:", row[0])
-                    print()
-                    print("Days:", row[1])
-                    print("Start time:", row[2])
-                    print("End time:", row[3])
-                    print("Building:", row[4])
-                    print("Room:", row[5])
-                    print()
-
-                    # run sql request for cross listings and print
-                    cursor.execute(cross_stmt,[args.classid])
+                # run sql request for professors and print
+                cursor.execute(profs_stmt,[args.classid])
+                row = cursor.fetchone()
+                while row is not None:
+                    print("Professor:", row[0])
                     row = cursor.fetchone()
-                    while row is not None:
-                        print("Dept and Number:", row[0],  row[1])
-                        row = cursor.fetchone()
-
-                    # run sql request for courses and print
-                    cursor.execute(course_stmt,[args.classid])
-                    row = cursor.fetchone()
-                    print()
-                    print("Area:", row[0])
-                    print()
-                    title = "Title: " + row[1]
-                    print(textwrap.fill(title, width = 72))
-                    print()
-                    descrip = "Description: " + row[2]
-                    print(textwrap.fill(descrip, width = 72))
-                    print()
-                    prereq = "Prerequisites: " + row[3]
-                    print(textwrap.fill(prereq, width = 72))
-                    print()
-
-                    # run sql statement for professors and print
-                    cursor.execute(profs_stmt,[args.classid])
-                    row = cursor.fetchone()
-                    while row is not None:
-                        print("Professor:", row[0])
-                        row = cursor.fetchone()
     # catch errors
     except RuntimeError as ex:
-        print(sys.argv[0] + ":",  'no class with classid', args.classid, 'exists', file=stderr)
+        print(sys.argv[0] + ":", "no class with classid", 
+            args.classid, "exists", file=stderr)
     except Exception as ex:
         print(ex, file=stderr)
         exit(1)
